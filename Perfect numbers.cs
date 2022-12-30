c6711668b45185d33e2ca26459d64e5e9da3c08e
MennahElsayed89 <121667815+MennahElsayed89@users.noreply.github.com>
2022-12-30 12:25:40 +0200

    Add files via upload

---
+++ Perfect numbers.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+ // Compiler version 4.0, .NET Framework 4.5
+
+
+ namespace Dcoder
+ {
+   public class Program
+   {
+     public static void Main(string[] args)
+     {
+       int sum ; 
+        Console.WriteLine("Please,Enter the start of the range :");
+        int start = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please,Enter the end of the range : ");
+        int end = int.Parse(Console.ReadLine());
+        
+      for (int i=start; i<=end;i++)
+      {
+        sum=0;
+        for (int factor=1; factor<i; factor++)
+        {
+          if(i%factor==0)
+          {
+            sum=sum+factor;
+          }
+        }
+        
+        if(sum==i)
+         {
+          Console.WriteLine(i);
+         }
+      }
+     }
+   }
+ }
---
+++ Prime numbers-1.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+ // Compiler version 4.0, .NET Framework 4.5
+
+
+ namespace Dcoder
+ {
+   public class Program
+   {
+     public static void Main(string[] args)
+     {
+       Console.WriteLine("Please , Enter the first  number :");
+
+
+
+      int num1 = int.Parse(Console.ReadLine());
+
+      Console.WriteLine("Please, Enter the second number : ");
+      int num2 = int.Parse(Console.ReadLine());
+      for (int i = num1; i <= num2; i++)
+      {
+        bool isprime = true;
+
+        for (int j = 2; j < i; j++)
+        {
+          if (i % j == 0)
+          {
+            isprime = false;
+          }
+        }
+        if (isprime == true && i > num1)
+        {
+          Console.WriteLine(i);
+        }
+
+      }
+     }
+   }
+ }
